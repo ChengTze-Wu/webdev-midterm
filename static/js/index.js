b4419de0828db6d0a930d3809c5214b3f6ab7a14
ChengTze-Wu <yaoop3050777@gmail.com>
2022-04-13 17:16:15 +0800

    add dark mode

--- static/js/index.js
+++ static/js/index.js
@@ -13,12 +13,33 @@ function linkToPage() {
     const nav_items = document.querySelectorAll(".nav_item");
     nav_items.forEach((nav_item) => {
         nav_item.addEventListener("click", async () => {
-            const content = await getPage(
-                `/webdev-midterm/${nav_item.id}.html`
-            );
+            // const content = await getPage(
+            //     `/webdev-midterm/${nav_item.id}.html`
+            // );
+            const content = await getPage(`./${nav_item.id}.html`);
             renderToRoot(content);
         });
     });
 }
 
+function switchMode() {
+    const body = document.querySelector("body");
+    const toggle = document.getElementById("toggle");
+    window.addEventListener("load", () => {
+        if (toggle.checked) {
+            body.classList.add("dark");
+        } else {
+            body.classList.remove("dark");
+        }
+    });
+    toggle.addEventListener("click", (e) => {
+        if (e.target.checked) {
+            body.classList.add("dark");
+        } else {
+            body.classList.remove("dark");
+        }
+    });
+}
+
 linkToPage();
+switchMode();
